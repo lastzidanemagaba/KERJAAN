431b8bf1f5d1e5ee7564340ce02928da7dcc6b17
lastzidanemagaba <lastzidanemagaba@gmail.com>
2022-12-08 19:41:13 +0700

    first

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+
+
+$file = "DataDoaSai.txt";
+$content = file_get_contents($file);
+$value = "";
+$alphas = range('A', 'Z');
+$ya = explode("\n", $content);
+//remove spaces from array elements and remove empty elements 
+$ya2 = array_map('trim', $ya);
+//print_r($ya2);
+
+
+$matches  = preg_grep ('/^.Doa (\w+)/i', $ya2);
+$rep = str_replace(".", "", $matches);
+$masukindb = (array_values($rep));
+// print_r($masukindb);
+
+// insert database
+$servername = "localhost";
+$username = 'zidane';
+$password = 'zidane';
+$dbname = 'jtindode_qtracking';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+for($i=0;$i<count($masukindb);$i++)
+{
+$sql = "INSERT INTO `panduan_umroh_subbab` (`id`, `panduan_umroh_bab`, `title_id`, `title_ar`) VALUES (NULL, '2', '$masukindb[$i]', NULL);";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully"."<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+}
+
+
+
+
+?>
